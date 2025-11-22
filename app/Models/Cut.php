8b01846438881c354a281b6e5383f6997558e4c4
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-23 02:00:25 +0700

    feat: create Cut.php

---
+++ app/Models/Cut.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cut extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'notes',
+    ];
+}
