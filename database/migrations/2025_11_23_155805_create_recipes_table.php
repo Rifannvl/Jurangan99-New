5c420d63db546079cf4516a55800e66da65eec7a
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-23 23:15:50 +0700

    feat: create 2025_11_23_155805_create_recipes_table.php

---
+++ database/migrations/2025_11_23_155805_create_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('excerpt')->nullable();
+            $table->text('body');
+            $table->string('product_link')->nullable();
+            $table->string('product_link_text')->default('Beli sekarang');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
