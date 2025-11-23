d527b9286fa0b6f06b0f2df9aca3124c32a68576
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-23 22:55:37 +0700

    update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -78,5 +79,10 @@ class User extends Authenticatable
     public function hasRole(string $role): bool
     {
         return (string) $this->role === $role;
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
     }
 }
